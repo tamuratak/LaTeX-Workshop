8dc2b73067b8ddc204f829fd5ddda3524047790f
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-04-14 20:02:49 +0900

    convert docs of unimath to markdown.

--- dev/unimathsymbols.py
+++ dev/unimathsymbols.py
@@ -6,24 +6,45 @@ The result is used to generate command intellisense for LaTeX Workshop.
 '''
 
 import json
+import re
+from typing import List, Set, Dict, Tuple, Union, Optional, Match
+
+
+def to_markdown(doc: str) -> str:
+    arry: List[str] = []
+    cmd = r'[=#xt]\s+(\\\w+(\{[\\\w\{\}]+\})?\s*(\(.*?\))?)\s*,?'
+    for m in re.finditer(cmd, doc):
+        arry.append('- ' + m.group(1))
+    doc = re.sub(cmd, '', doc)
+    doc = doc.strip()
+
+    def capitalize(m: Match) -> str:
+        return m.group(0).capitalize()
+
+    doc = re.sub(r'[A-Z]+', capitalize, doc)
+    if doc == '':
+        return '\n'.join(arry)
+    else:
+        return '- ' + doc + '\n' + '\n'.join(arry)
+
 
 data = {}
 
 with open('unimathsymbols.txt', encoding='utf-8') as f:
     for line in f:
-        if line[0] is '#':
+        if line[0] == '#':
             continue
         segments = line.split('^')
-        if segments[3] is '':
+        if segments[3] == '':
             continue
-        if segments[3][0] is '\\':
+        if segments[3][0] == '\\':
             segments[3] = segments[3][1:]
         data[segments[3]] = {
             'command': segments[3],
             'detail': segments[1],
-            'documentation': segments[7].strip().capitalize()
+            'documentation': to_markdown(segments[7].strip())
         }
-        if segments[6] is not '' and segments[6][0] is not '-':
+        if segments[6] != '' and segments[6][0] != '-':
             data[segments[3]]['detail'] += ' ("{}" command)'.format(segments[6])
 
 json.dump(data, open('../data/unimathsymbols.json', 'w', encoding='utf-8'),

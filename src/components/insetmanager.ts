f6f3c4cda03d2e0e62496575fe3109ed99dd31f2
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-06-06 23:27:10 +0900

    add realtime math preview

--- src/components/insetmanager.ts
+++ src/components/insetmanager.ts
@@ -19,16 +19,34 @@ export class MathPreviewInsetManager {
             return
         }
         const document = editor.document
+        const position0 = editor.selection.active
+        const position1 = new vscode.Position(position0.line + 1, position0.character + 5)
         const prev = this.previewInsets.get(document)
         if (prev) {
             this.previewInsets.delete(document)
             prev.dispose()
         } else {
-            const range = new vscode.Range(0, 0, 0, 1)
+            const range = new vscode.Range(position0, position1)
             try {
-                const inset = vscode.window.createWebviewTextEditorInset(editor, range)
+                const inset = vscode.window.createWebviewTextEditorInset(editor, range, {enableScripts: true})
                 this.previewInsets.set(document, inset)
-                inset.webview.html = 'abc'
+                inset.webview.html = `<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; script-src 'unsafe-inline'; img-src data:; style-src 'unsafe-inline';">
+                    <meta charset="UTF-8">
+                    <script>
+                    window.addEventListener('message', event => {
+                        const message = event.data; // The JSON data our extension sent
+                        const img = document.getElementById('math');
+                        img.src = message;
+                    });
+                    </script>
+                </head>
+                <body>
+                    <div style="text-align:center;"><img src="" id="math" /></div>
+                </body>
+                </html>`
                 return inset
             } catch (e) {
                 console.log(e)
@@ -37,7 +55,7 @@ export class MathPreviewInsetManager {
         return
     }
 
-    updateMathPreviewInset(document: vscode.TextDocument) {
+    async updateMathPreviewInset(document: vscode.TextDocument) {
         const inset = this.previewInsets.get(document)
         if (!inset) {
             return
@@ -47,11 +65,14 @@ export class MathPreviewInsetManager {
             return
         }
         const position = editor.selection.active
-        const texMath = this.mathPreview.findMathEnvIncludingPosition(document, position)
+        let texMath = this.mathPreview.findMathEnvOnBeginEnvname(document, position)
+        if (!texMath) {
+            texMath = this.mathPreview.findMathEnvIncludingPosition(document, position)
+        }
         if (!texMath) {
             return
         }
-        const svgDataUrl = this.mathPreview.generateSVG(document, texMath)
+        const svgDataUrl = await this.mathPreview.generateSVG(document, texMath)
         return inset.webview.postMessage(svgDataUrl)
     }
 }

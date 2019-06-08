733a1ea68e092d5ead9a38377f9e61a4033d2c5a
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-06-09 08:22:09 +0900

    set delay only when mouse event.

--- src/main.ts
+++ src/main.ts
@@ -285,8 +285,11 @@ export async function activate(context: vscode.ExtensionContext) {
         await extension.mathPreviewInsetManager.updateMathPreviewInset(e.document)
     }))
     context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(async (e) => {
-        const configuration = vscode.workspace.getConfiguration('latex-workshop')
-        const delay = configuration.get('inset.mathpreview.delay') as number
+        let delay = 0
+        if (e.kind === vscode.TextEditorSelectionChangeKind.Mouse) {
+            const configuration = vscode.workspace.getConfiguration('latex-workshop')
+            delay = configuration.get('inset.mathpreview.delay') as number
+        }
         setTimeout( () => {
             extension.mathPreviewInsetManager.moveInsetIfNeeded(e.textEditor)
         }, delay)

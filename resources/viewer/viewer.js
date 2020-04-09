44df75bf7480d34780607770fc3b0925deb54063
Takashi Tamura <tamuratak@users.noreply.github.com>
2020-04-10 08:40:36 +0900

    edit

--- resources/viewer/viewer.js
+++ resources/viewer/viewer.js
@@ -3,25 +3,30 @@
 //
 // Note: this works on first load, or when navigating between groups, but not when
 //       navigating between tabs of the same group for some reason!
-const iframe = document.getElementById('preview-panel');
-window.onfocus = iframe.onload = function() {
-    setTimeout(function() { // doesn't work immediately
-        iframe.contentWindow.focus();
-    }, 100);
-}
+window.addEventListener('DOMContentLoaded ', () => {
+    const iframe = document.getElementById('preview-panel');
+    window.onfocus = iframe.onload = function() {
+        setTimeout(function() { // doesn't work immediately
+                   iframe.contentWindow.focus();
+                }, 100);
+            }
+})
 
 const vsStore = acquireVsCodeApi();
+console.log(vsStore);
 // vsStore.setState({path: '${pdfFile}'});
 // To enable keyboard shortcuts of VS Code when the iframe is focused,
 // we have to dispatch keyboard events in the parent window.
 // See https://github.com/microsoft/vscode/issues/65452#issuecomment-586036474
 window.addEventListener('message', (e) => {
+    console.log('messssggg!!')
     if (e.origin !== `http://localhost:${extensionServerPort}`) {
         return;
     }
     switch (e.data.type) {
         case 'initialized': {
             const status = vsStore.getState();
+            const iframe = document.getElementById('preview-panel');
             status.type = 'restore_status';
             iframe.contentWindow.postMessage(status, '*');
             break;

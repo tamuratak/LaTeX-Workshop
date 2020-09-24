ff061b93bd09361796a222c53b44c9d7f9ab0687
Takashi Tamura <tamuratak@users.noreply.github.com>
2020-09-24 17:15:54 +0900

    edit

--- src/providers/preview/mathpreview.ts
+++ src/providers/preview/mathpreview.ts
@@ -1,7 +1,4 @@
 import * as vscode from 'vscode'
-import * as fs from 'fs'
-import {latexParser} from 'latex-utensils'
-import * as path from 'path'
 
 import {MathJaxPool, TypesetArg} from './mathjaxpool'
 import * as utils from '../../utils/utils'
@@ -10,116 +7,25 @@ import {Extension} from '../../main'
 import {Suggestion as ReferenceEntry} from '../completer/reference'
 import {getCurrentThemeLightness} from '../../utils/theme'
 
+import {NewCommandFinder} from './mathpreviewlib/newcommandfinder'
+
 export type TexMathEnv = { texString: string, range: vscode.Range, envname: string }
 
 export class MathPreview {
     private readonly extension: Extension
     private color: string = '#000000'
     private readonly mj: MathJaxPool
+    private readonly newCommandFinder: NewCommandFinder
 
     constructor(extension: Extension) {
         this.extension = extension
         this.mj = new MathJaxPool()
         vscode.workspace.onDidChangeConfiguration(() => this.getColor())
+        this.newCommandFinder = new NewCommandFinder(extension)
     }
 
-    private postProcessNewCommands(commands: string): string {
-        return commands.replace(/\\providecommand/g, '\\newcommand')
-                       .replace(/\\newcommand\*/g, '\\newcommand')
-                       .replace(/\\renewcommand\*/g, '\\renewcommand')
-                       .replace(/\\DeclarePairedDelimiter{(\\[a-zA-Z]+)}{([^{}]*)}{([^{}]*)}/g, '\\newcommand{$1}[2][]{#1$2 #2 #1$3}')
-    }
-
-    private async loadNewCommandFromConfigFile(newCommandFile: string) {
-        let commandsString = ''
-        if (newCommandFile === '') {
-            return commandsString
-        }
-        if (path.isAbsolute(newCommandFile)) {
-            if (fs.existsSync(newCommandFile)) {
-                commandsString = fs.readFileSync(newCommandFile, {encoding: 'utf8'})
-            }
-        } else {
-            if (this.extension.manager.rootFile === undefined) {
-                await this.extension.manager.findRoot()
-            }
-            const rootDir = this.extension.manager.rootDir
-            if (rootDir === undefined) {
-                this.extension.logger.addLogMessage(`Cannot identify the absolute path of new command file ${newCommandFile} without root file.`)
-                return ''
-            }
-            const newCommandFileAbs = path.join(rootDir, newCommandFile)
-            if (fs.existsSync(newCommandFileAbs)) {
-                commandsString = fs.readFileSync(newCommandFileAbs, {encoding: 'utf8'})
-            }
-        }
-        commandsString = commandsString.replace(/^\s*$/gm, '')
-        commandsString = this.postProcessNewCommands(commandsString)
-        return commandsString
-    }
-
-    async findProjectNewCommand(ctoken: vscode.CancellationToken): Promise<string> {
-        const configuration = vscode.workspace.getConfiguration('latex-workshop')
-        const newCommandFile = configuration.get('hover.preview.newcommand.newcommandFile') as string
-        let commandsInConfigFile = ''
-        if (newCommandFile !== '') {
-            commandsInConfigFile = await this.loadNewCommandFromConfigFile(newCommandFile)
-        }
-
-        if (!configuration.get('hover.preview.newcommand.parseTeXFile.enabled')) {
-            return commandsInConfigFile
-        }
-        let commands: string[] = []
-        let exceeded = false
-        setTimeout( () => { exceeded = true }, 5000)
-        for (const tex of this.extension.manager.getIncludedTeX()) {
-            if (ctoken.isCancellationRequested) {
-                return ''
-            }
-            if (exceeded) {
-                this.extension.logger.addLogMessage('Timeout error when parsing preambles in findProjectNewCommand.')
-                throw new Error('Timeout Error in findProjectNewCommand')
-            }
-            const content = this.extension.manager.cachedContent[tex].content
-            commands = commands.concat(await this.findNewCommand(content))
-        }
-        return commandsInConfigFile + '\n' + this.postProcessNewCommands(commands.join(''))
-    }
-
-    private async findNewCommand(content: string): Promise<string[]> {
-        let commands: string[] = []
-        try {
-            const ast = await this.extension.pegParser.parseLatexPreamble(content)
-            const regex = /((re)?new|provide)command(\\*)?|DeclareMathOperator(\\*)?/
-            for (const node of ast.content) {
-                if (((latexParser.isCommand(node) && node.name.match(regex)) || latexParser.isDefCommand(node)) && node.args.length > 0) {
-                    const s = latexParser.stringify(node)
-                    commands.push(s)
-                } else if (latexParser.isCommand(node) && node.name === 'DeclarePairedDelimiter' && node.args.length === 3) {
-                    const name = latexParser.stringify(node.args[0])
-                    const leftDelim = latexParser.stringify(node.args[1]).slice(1, -1)
-                    const rightDelim = latexParser.stringify(node.args[2]).slice(1, -1)
-                    const s = `\\newcommand${name}[2][]{#1${leftDelim} #2 #1${rightDelim}}`
-                    commands.push(s)
-                }
-            }
-        } catch (e) {
-            commands = []
-            const regex = /(\\(?:(?:(?:(?:re)?new|provide)command|DeclareMathOperator)(\*)?{\\[a-zA-Z]+}(?:\[[^[\]{}]*\])*{.*})|\\(?:def\\[a-zA-Z]+(?:#[0-9])*{.*})|\\DeclarePairedDelimiter{\\[a-zA-Z]+}{[^{}]*}{[^{}]*})/gm
-            const noCommentContent = content.replace(/([^\\]|^)%.*$/gm, '$1') // Strip comments
-            let result: RegExpExecArray | null
-            do {
-                result = regex.exec(noCommentContent)
-                if (result) {
-                    let command = result[1]
-                    if (result[2]) {
-                        command = command.replace(/\*/, '')
-                    }
-                    commands.push(command)
-                }
-            } while (result)
-        }
-        return commands
+    findProjectNewCommand(ctoken: vscode.CancellationToken): Promise<string> {
+        return this.newCommandFinder.findProjectNewCommand(ctoken)
     }
 
     private addDummyCodeBlock(md: string): string {
@@ -251,7 +157,7 @@ export class MathPreview {
     }
 
     async generateSVG(document: vscode.TextDocument, tex: TexMathEnv, newCommands0?: string) {
-        const newCommands: string = newCommands0 ?? (await this.findNewCommand(document.getText())).join('')
+        const newCommands: string = newCommands0 ?? (await this.newCommandFinder.findNewCommand(document.getText())).join('')
         const configuration = vscode.workspace.getConfiguration('latex-workshop')
         const scale = configuration.get('hover.preview.scale') as number
         const s = this.mathjaxify(tex.texString, tex.envname)
